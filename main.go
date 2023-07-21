c3c578acc32bb128f4c02bc838a8f25c7a10df57
Mahdi Khosravi <mahdi.khosravi@metu.edu.tr>
2023-07-21 14:36:32 +0300

    task2 done

---
+++ main.go
@@ -0,0 +1,148 @@
+package main
+
+import (
+	"database/sql"
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"strconv"
+
+	_ "github.com/lib/pq"
+)
+
+const INTERFACE = "127.0.0.1"
+const PORT = "3000"
+const SERVER = INTERFACE + ":" + PORT
+
+type Spot struct {
+	ID          string         `json:"id"`
+	Name        string         `json:"name"`
+	Website     sql.NullString `json:"website"`
+	Coordinates string         `json:"coordinates"`
+	Description sql.NullString `json:"description"`
+	Rating      float64        `json:"rating"`
+	Distance    float64        `json:"distance,omitempty"`
+}
+
+type SpotsResponse struct {
+	Spots []Spot `json:"spots"`
+}
+
+const (
+	DbDriver   = "postgres"
+	DbUser     = "postgres"
+	DbPassword = "postgres"
+	DbName     = "spotdb"
+)
+
+func main() {
+	http.HandleFunc("/", spotsReqHandler)
+	log.Fatal(http.ListenAndServe(SERVER, nil))
+}
+
+func spotsReqHandler(w http.ResponseWriter, r *http.Request) {
+	locationType := r.URL.Query().Get("type")
+	if locationType != "circle" && locationType != "square" {
+		http.Error(w, "Invalid location type. Valid types are 'circle' and 'square'", http.StatusBadRequest)
+		return
+	}
+
+	radius, err := strconv.ParseFloat(r.URL.Query().Get("radius"), 64)
+	if err != nil {
+		http.Error(w, "Wrong radius", http.StatusBadRequest)
+		return
+	}
+
+	latitude, err := strconv.ParseFloat(r.URL.Query().Get("latitude"), 64)
+	if err != nil {
+		http.Error(w, "Wrong latitude", http.StatusBadRequest)
+		return
+	}
+
+	longitude, err := strconv.ParseFloat(r.URL.Query().Get("longitude"), 64)
+	if err != nil {
+		http.Error(w, "Wrong longitude", http.StatusBadRequest)
+		return
+	}
+
+	spots, err := getSpots(latitude, longitude, radius, locationType)
+	if err != nil {
+		http.Error(w, "Error retrieving spots", http.StatusInternalServerError)
+		return
+	}
+
+	response := SpotsResponse{Spots: spots}
+	jsonResponse, err := json.Marshal(response)
+	if err != nil {
+		http.Error(w, "Error creating JSON response", http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(jsonResponse)
+}
+
+func getSpots(latitude, longitude, radius float64, locationType string) ([]Spot, error) {
+	db, err := sql.Open(DbDriver, fmt.Sprintf("host=localhost port=5432 user=%s password=%s dbname=%s sslmode=disable", DbUser, DbPassword, DbName))
+	if err != nil {
+		fmt.Println(err)
+		return nil, err
+	}
+	defer db.Close()
+
+	query := ""
+	switch locationType {
+	case "circle":
+		query = fmt.Sprintf(`
+			SELECT
+				id, name, website, ST_AsText(coordinates), description, rating
+			FROM
+				"MY_TABLE"
+			WHERE
+				ST_DWithin(coordinates::geography, 'SRID=4326;POINT(%f %f)', %f)
+			ORDER BY
+				CASE
+					WHEN ST_Distance(coordinates::geography, 'SRID=4326;POINT(%f %f)') < 50
+					THEN rating
+					ELSE ST_Distance(coordinates::geography, 'SRID=4326;POINT(%f %f)')
+				END;
+		`, longitude, latitude, radius, longitude, latitude, longitude, latitude)
+	case "square":
+		query = fmt.Sprintf(`
+			SELECT
+				id, name, website, ST_AsText(coordinates), description, rating
+			FROM
+				"MY_TABLE"
+			WHERE
+				ST_DWithin(coordinates::geography, ST_MakeEnvelope(%f, %f, %f, %f, 4326), 0)
+			ORDER BY
+				CASE
+					WHEN ST_Distance(coordinates::geography, 'SRID=4326;POINT(%f %f)') < 50
+					THEN rating
+					ELSE ST_Distance(coordinates::geography, 'SRID=4326;POINT(%f %f)')
+				END;
+		`, longitude, latitude, longitude-radius, latitude-radius, longitude+radius, latitude+radius, longitude, latitude)
+	}
+
+	rows, err := db.Query(query)
+	if err != nil {
+		fmt.Println(err)
+		return nil, err
+	}
+	defer rows.Close()
+
+	spots := []Spot{}
+	for rows.Next() {
+		var spot Spot
+		err := rows.Scan(&spot.ID, &spot.Name, &spot.Website, &spot.Coordinates, &spot.Description, &spot.Rating)
+		if err != nil {
+			fmt.Println(err)
+			return nil, err
+		}
+		spots = append(spots, spot)
+	}
+
+	return spots, nil
+}
